2614d1417b6e36e8d4fecb35a9b551fc0c7f04db
Dmytro Puchkov <ragalipelican123@gmail.com>
2023-12-06 22:33:56 +0400

    Added dummy project

---
+++ IT-Nanny/Program.cs
@@ -0,0 +1,33 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace IT_Nanny;
+
+public class Program
+{
+    private static DiscordSocketClient _client;
+
+    public static async Task Main(string[] args)
+    {
+        _client = new DiscordSocketClient();
+
+        _client.Log += Log;
+
+        var token = "MTE3OTgxOTQzNDUyNTg2ODE1Mg.GDOsE-.cZvyovACd33dlTAoZKpYwffZL12jKB3pGpdpkQ";
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+        _client.MessageReceived += async (m) =>
+        {
+            await m.Channel.SendMessageAsync("Dungeon Master");
+        };
+        
+        await Task.Delay(-1);
+    }
+    
+    private static Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+}
